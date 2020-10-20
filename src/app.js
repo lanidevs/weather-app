acac601829efee1385bec17aa5cf0901e263b678
Leilani Speer <leilanispeer@gmail.com>
2020-10-20 13:15:00 -0700

    Udapted the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours <10) {
+        hours=`0${hours}`;
+    }
+    let minutes =date.getMinutes();
+    if (minutes <10) {
+        minutes=`0${minutes}`;
+    }
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    let day= days[date.getDay()];
+
+ return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 console.log(response.data);
     let temperatureElement= document.querySelector("#temperature");
@@ -5,6 +21,8 @@ console.log(response.data);
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
+    let dateElement=document.querySelector("#date");
+
 
 
 
@@ -13,6 +31,8 @@ console.log(response.data);
     descriptionElement.innerHTML= response.data.weather[0].description;
     humidityElement.innerHTML= response.data.main.humidity;
     windElement.innerHTML= Math.round (response.data.wind.speed);
+    dateElement.innerHTML= formatDate(response.data.dt *1000);
+
 
 }
 
