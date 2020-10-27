2dfb762ba214c8f986ee1fedb877f872e62a3d2c
Leilani Speer <leilanispeer@gmail.com>
2020-10-26 20:46:32 -0700

    Added 3-hour segment forecast(s)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 function formatDate(timestamp) {
     let date = new Date(timestamp);
+    
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    let day= days[date.getDay()];
+
+ return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp){
+    let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours <10) {
         hours=`0${hours}`;
@@ -10,9 +19,9 @@ function formatDate(timestamp) {
     }
     let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
     let day= days[date.getDay()];
-
- return `${day} ${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 }
+
 
 function displayTemperature(response) {
     let temperatureElement= document.querySelector("#temperature");
@@ -37,12 +46,36 @@ function displayTemperature(response) {
 
 }
 
+function displayForecast(response){
+    let forecastElement=document.querySelector("#forecast");
+    forecastElement.innerHTML=null;
+    let forecast= null;
+    
+    for (let index=0; index < 6 ; index ++) {
+        let forecast= response.data.list[index];
+        forecastElement.innerHTML += `<div class="col-2">
+    <h3>
+        ${formatHours(forecast.dt*1000)}
+    </h3>
+    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
+    <div class="weather-forecast-temperature">
+        <strong>${Math.round(forecast.main.temp_max)}°</strong>/
+        ${Math.round(forecast.main.temp_min)}°
+    </div>
+</div>
+   ` } 
+    
+}
 
 function search(city){
     let apiKey ="ca4ff62b82ce5d50cb085203c2cfa005";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
+
+    apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
+
 function handleSubmit(event){
  event.preventDefault();
  let cityInputElement= document.querySelector("#city-input");
